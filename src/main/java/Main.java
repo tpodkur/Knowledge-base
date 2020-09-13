b2e499b58efaba88462c096a3d9acef4c62f3abe
Tatyana Podkur <t.podkur@g.nsu.ru>
2020-09-13 15:15:36 +0700

    Java parser

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,117 +1,63 @@
-import com.github.igorsuhorukov.phantomjs.PhantomJsDowloader;
-import jodd.io.NetUtil;
-import jodd.util.SystemUtil;
-import org.jsoup.*;
-import org.jsoup.select.*;
-import org.jsoup.nodes.*;
+import java.util.ArrayList;
+import java.util.List;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.util.concurrent.TimeUnit;
+import java.util.Date;
 
-import java.io.*;
-import java.net.URL;
-
-import com.gargoylesoftware.htmlunit.WebClient;
-import com.gargoylesoftware.htmlunit.html.HtmlPage;
-import java.io.IOException;
 
 public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        String url = "https://stepik.org/catalog?tag=866";
+        CoursesSetParser setParser = new CoursesSetParser();
+        setParser.loadCourses(url, "section");
+    }
 
-    public static void main(String[] args) {
+    public void parser() throws InterruptedException {
+        String url = "https://stepik.org/catalog";
+        System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
+        WebDriver driver = new ChromeDriver();
 
-//        try {
-//            WebClient client = new WebClient();
-//            HtmlPage page = client.getPage("https://stepik.org/catalog");
-//
-//            System.out.println(page);
-//        } catch (IOException e) {
-//            System.out.println("1");
-//        }
-//
-//        try {
-//            File file = new File("google.html");
-//            NetUtil.downloadFile("https://stepik.org/catalog", file);
-//
-//            System.out.println(file);
-//        } catch (IOException e) {
-//
-//        }
+//        driver.manage().timeouts().implicitlyWait(240, TimeUnit.SECONDS);
+        driver.get(url);
 
-
-//        try {
-//            URL u = new URL("https://stepik.org/catalog");
-//            InputStream in = u.openStream();
-//            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-//            StringBuilder result = new StringBuilder();
-//            String line;
-//            while((line = reader.readLine()) != null) {
-//                result.append(line);
+//        WebDriverWait wait = new WebDriverWait(driver, 60);
+//        wait.until(new ExpectedCondition<Boolean>() {
+//            public Boolean apply(WebDriver wdriver) {
+//                return ((JavascriptExecutor) driver).executeScript(
+//                        "return document.readyState"
+//                ).equals("complete");
 //            }
-//            System.out.println(result.toString());
-//        }catch (IOException e) {
-//
-//        }
+//        });
+        TimeUnit.SECONDS.sleep(30);
 
-        try {
-            Document doc = Jsoup.connect("https://stepik.org/catalog")
-                    .userAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36")
-                    .get();
-            String text = doc.body().html();
-            String html = doc.outerHtml();
-            System.out.println(html);
+        List<WebElement> courseSets = driver
+                .findElement(By.className("st-course-filters"))
+                .findElements(By.className("st-filter"));
 
-//            Element main = doc.getElementsByTag("main").first();
-//            Element section = main.getElementsByTag("section").first();
-//            Elements ols = section.getElementsByClass("course-pack ember-view");
-//
-//            Element firstOl = ols.first();
-//            Elements refers = firstOl.getElementsByTag("a");
-//            for (Element link : refers) {
-//                String linkHref = link.attr("href");
-//                System.out.println(linkHref);
-//            }
+        List<Link> courseLinks = new ArrayList<Link>();
+        for (WebElement courseSet : courseSets) {
+            List<WebElement> listItems = courseSet.findElements(By.className("st-filter__item"));
+            for (WebElement listItem : listItems) {
+                WebElement linkElement = listItem.findElement(By.className("st-filter__link"));
+                String link = linkElement.getAttribute("href");
+                String section = linkElement.getText();
 
-        } catch (IOException e) {
+                Link newLink = new Link(link, section);
+                courseLinks.add(newLink);
+            }
         }
 
-        String phantomJsPath = PhantomJsDowloader.getPhantomJsPath();
-        WebDriver driver = new PhantomJSDriver(settings)
+        CoursesSetParser setParser = new CoursesSetParser();
+        for (Link courseLink : courseLinks) {
+            setParser.loadCourses(courseLink.getLink(), courseLink.getSection());
+        }
+
+        driver.quit();
     }
 }
-
-//import com.gargoylesoftware.htmlunit.WebClient;
-//import com.gargoylesoftware.htmlunit.html.HtmlPage;
-//import java.io.IOException;
-//
-//        try {
-//            WebClient client = new WebClient();
-//            HtmlPage page = client.getPage("https://newyork.craigslist.org/search/sss?sort=rel&query=iphone+6s");
-//
-//            System.out.println(page);
-//        } catch (IOException e) {
-//            System.out.println("1");
-//        }
-
-
-//import org.jsoup.*;
-//import org.jsoup.select.*;
-//import org.jsoup.nodes.*;
-//import java.io.IOException;
-
-//        try {
-//            Document doc = Jsoup.connect("https://stepik.org/catalog?verb").get();
-//            String text = doc.body().html();
-//            String html = doc.outerHtml();
-//            System.out.println(html);
-//
-//            Element main = doc.getElementsByTag("main").first();
-//            Element section = main.getElementsByTag("section").first();
-//            Elements ols = section.getElementsByClass("course-pack ember-view");
-//
-//            Element firstOl = ols.first();
-//            Elements refers = firstOl.getElementsByTag("a");
-//            for (Element link : refers) {
-//                String linkHref = link.attr("href");
-//                System.out.println(linkHref);
-//            }
-//
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
