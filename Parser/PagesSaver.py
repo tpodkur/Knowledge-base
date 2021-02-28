9dc07008d9a9a394e28ac5792374f51576f6573f
Tatyana Podkur <t.podkur17@gmail.com>
2021-02-28 13:22:25 +0700

    save courses links to files

--- Parser/PagesSaver.py
+++ Parser/PagesSaver.py
@@ -1,4 +1,5 @@
 import string
+from lxml import html
 import requests
 
 
@@ -10,11 +11,40 @@ class PagesSaver:
         self.driver = None
 
     def run(self):
-        return True
+        pageNumber = 1
+        url = "https://online.edu.ru/public/courses?faces-redirect=true&pid=2919"
+        platform = "Stepik"
+        page = requests.get(url + "&page=" + str(pageNumber))
+        tree = html.fromstring(page.content)
 
-    def loadCoursesPages(self, url: string, platform: string):
+        root = html.parse("CoursesListPages/Stepik/page0.html").getroot()
+        elements = root.find_class("card course-card")
+        for element in elements:
+            print(element[1][0][0].get('href'))
+
+        fileToWrite = open("CoursesListPages/" + str(platform) + "/page" + str(pageNumber) + ".html", "w")
+        # fileToWrite.write(tree)
+        fileToWrite.close()
+
+    def loadCoursesListPages(self, url: string, platform: string):
         for pageNumber in range(20):
             page = requests.get(url + "&page=" + str(pageNumber))
-            fileToWrite = open("CoursesPages/" + str(platform) + "/page" + str(pageNumber) + ".html", "w")
+            fileToWrite = open("CoursesListPages/" + platform + "/page" + str(pageNumber) + ".html", "w")
             fileToWrite.write(page.text)
+            fileToWrite.close()
+
+    def getCoursesLinksFromPages(self, platform: string):
+        for i in range(20):
+            root = html.parse("CoursesListPages/" + platform + "/page" + str(i) + ".html").getroot()
+            elements = root.find_class("card course-card")
+
+            # links = []
+            links = ""
+            for element in elements:
+                # links.append(element[1][0][0].get('href'))
+                links = links + str(element[1][0][0].get('href')) + "\n"
+
+            fileToWrite = open("CoursesLinks/" + platform + ".txt", "w")
+            # fileToWrite.writelines(links)
+            fileToWrite.write(links)
             fileToWrite.close()
--- main.py
+++ main.py
@@ -8,7 +8,14 @@ if __name__ == '__main__':
     courseraCoursesUrl = "/public/courses?faces-redirect=true&pid=11042928"
     openEdu = "/public/courses?faces-redirect=true&pid=3114"
 
-    coursera_parser = PagesSaver()
-    coursera_parser.loadCoursesPages(baseUrl + stepikCoursesUrl, "Stepik")
-    coursera_parser.loadCoursesPages(baseUrl + courseraCoursesUrl, "Coursera")
-    coursera_parser.loadCoursesPages(baseUrl + openEdu, "OpenEdu")
+    saver = PagesSaver()
+    # saver.loadCoursesListPages(baseUrl + stepikCoursesUrl, "Stepik")
+    # saver.loadCoursesListPages(baseUrl + courseraCoursesUrl, "Coursera")
+    # saver.loadCoursesListPages(baseUrl + openEdu, "OpenEdu")
+
+    # saver.run()
+
+    saver.getCoursesLinksFromPages("Stepik")
+    saver.getCoursesLinksFromPages("Coursera")
+    saver.getCoursesLinksFromPages("OpenEdu")
+
